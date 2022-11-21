605769ae75a60fdee665a51e6dc31d7fc0775b8f
jesus-orduno <jesus.orduno@gmail.com>
2022-11-21 11:39:34 -0700

    Added initialize function

--- Develop/index.js
+++ Develop/index.js
@@ -61,7 +61,13 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(function (data) {
+      writeToFile('generatedREADME.md', generateMarkdown(data));
+    });
+}
 
 // Function call to initialize app
 init();
